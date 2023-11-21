a852a641463daac2e795644208233d2cb07b970e
OwaisAttaey <owais.att.2014@gmail.com>
2023-11-21 15:59:34 +0100

    the remove button in cart is functional now

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -29,6 +29,27 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
+    product = get_object_or_404(Product, id=product_id) # get the product
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart    = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+    
+
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -36,16 +57,18 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (10 * total) / 100
+        grand_total = total + tax
     except ObjectDoesNotExist:
         pass
     
     context = {
-        'total': total,
-        'quantity': quantity,
-        'cart_items': cart_items,
+        'total'       : total,
+        'quantity'    : quantity,
+        'cart_items'  : cart_items,
+        'tax'         : tax,
+        'grand_total' : grand_total,
     }
-        
-        
     return render(request, 'store/cart.html', context)
 
 
